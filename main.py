1e495b5861465eca6667f111d49f2f1de7d1e01c
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-18 20:59:12 +0500

    another commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 programming_dictionaries = {
     "Bug": "An error in a program that prevents the program from running as expected",
     "Program": "Set of rules and techniques that tells the computer what to do and how to do",
-    "Loop": "Repetition of something is called loop",
 }
 
-print(programming_dictionaries["Loop"])
+print(programming_dictionaries)
+# to add the key and value in the dictionary we use....
+programming_dictionaries["Loop"] = "repetition of something is called loop"
+
+print(programming_dictionaries)
