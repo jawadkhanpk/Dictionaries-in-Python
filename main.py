ccb90f197af1a72476eae45519ad7385c84f5ab2
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-18 20:14:53 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+programming_dictionaries = {
+    "Bug": "An error in a program that prevents the program from running as expected",
+    "Program": "Set of rules and techniques that tells the computer what to do and how to do",
+    "Loop": "Repetition of something is called loop",
+}
+
+print(programming_dictionaries["Loop"])
